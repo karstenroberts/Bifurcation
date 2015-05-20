d44160b991310a97cd07c1aa473e08a4b45f59e5
Karsten Roberts <karstenroberts@gmail.com>
2015-05-19 18:50:42 -0700

    1 period section
    Range from r=1 -> r=3

--- bifurcationDiagram.java
+++ bifurcationDiagram.java
@@ -1,15 +1,17 @@
+import java.awt.*;
 import java.util.*;
 public class bifurcationDiagram{
 
-   private static double r = 3.0;
+   private static double r = 1.1;
    private static double x = 0.0;
-   private static ArrayList<Double> values = new ArrayList<Double>();
-   
+   private static ArrayList<Point> values = new ArrayList<Point>();   
    
    public static void main(String[] args){
-      for(double ii = x; Math.abs(functionDerivative(ii)) < 1; ii+=.1){
-         values.add(function(ii));
-      }
+      
+      DrawingPanel panel = new DrawingPanel(600,300);    //pixelValue = actualValue*100*1.5 
+      Graphics g = panel.getGraphics();
+      
+      iterator(g);
    }
    
    public static double function(double x){
@@ -18,5 +20,36 @@ public class bifurcationDiagram{
    
    public static double functionDerivative(double x){
       return (r*(1-2*x));
+   }
+   
+   // public static double stableValue(r){
+//       return 1-(1-r);
+//    }
+   
+   public static void iterator(Graphics g){
+      
+      double stableX = 1-(1/r);
+      // while(Math.abs(functionDerivative(stableX))<1){
+//          stableX = 1-(1/r);
+//          values.add(new Point((r), function(stableX)));
+//          r+=.1;
+//       }
+      pointCreator(stableX);
+      for (int ii = 0; ii < values.size(); ii++){
+         int xPixel = (int)(values.get(ii).getX()*150);
+         System.out.println(xPixel);
+         int yPixel = (int)(values.get(ii).getY()*150);
+         System.out.println(300-yPixel);
+         g.drawRect(xPixel, 300-yPixel, 2,2);
+      }
+   }
+   
+   public static void pointCreator(double stableX){
+      while(Math.abs(functionDerivative(stableX))<1){
+         stableX = 1-(1/r);
+         values.add(new Point((r), function(stableX)));
+         r+=.1;
+      }
+      //pointCreator(function(stableX));
    }
 }
